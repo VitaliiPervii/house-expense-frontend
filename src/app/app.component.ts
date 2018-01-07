1590c3b9d14566345a339096139de7a67cb96fcc
Angular CLI <angular-cli@angular.io>
2018-01-07 15:14:24 +0200

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pvv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pvv';
+}
